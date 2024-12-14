488a562b925768dcb45722aa30dfb6ebe450a112
Paul Lecomte <164857566+Paul-Lecomte@users.noreply.github.com>
2024-12-14 11:39:20 +0100

    i now have a real frontend hurray

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react()],
   css: {
     postcss: {
-      plugins: [tailwindcss(), require('autoprefixer')],
+      plugins: [tailwindcss()],
     },
   },
 });
